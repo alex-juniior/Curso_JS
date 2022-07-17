fb17efb67d527531062c33bb35db028a3726df3a
Alex Júnior <alexjunior651@hotmail.com>
2022-07-16 23:43:41 -0300

    Declarando funções

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
-/*
-Comentários em JS // ou /* 
+function soma(valor1, valor2) {
+    return valor1 + valor2;
+}
 
-Vaviáveis em JS
+function realParaDolar(real, cotacaoDolar) {
+    return real * cotacaoDolar;
+}
 
--> var
--> let
--> const
+let total = realParaDolar(10, 5.09);
+alert(total);
+document.getElementById("texto").innerHTML = soma(10, 200);
 
-*/
+function paraCelsius(valorFahrenheit) {
+    return (5 / 9) * (valorFahrenheit - 32);
+}
 
-//DECLARAÇÃO DE VARIAVEIS
-let nome, sobreNome;
-
-//ATRIBUIÇÃO DOS VALORES
-nome = "Alex";
-sobreNome = "Green";
-document.getElementById("texto").innerHTML = nome + " " + sobreNome;
+let x = paraCelsius(77);
+alert(" A temperatura é de " + x + " graus celsius!");
