d6226070d155bdb74b7a5a1718c69465e722daf2
Alex Júnior <alexjunior651@hotmail.com>
2022-07-23 16:05:11 -0300

    Trabalhando com arrays

--- js/index.js
+++ js/index.js
@@ -1,27 +1,42 @@
-function eventoClick() {
-    alert("Acionou um evento de click!")
-    document.body.style.backgroundColor = "yellow";
-}
+const lista = ["arroz", "feijão", "macarrão", "leite"];
+console.log(lista[0]);
 
-function eventoDblClick() {
-    alert("Evento de clique dublo");
-}
+const lista1 = [];
+lista1[0] = "arroz";
+lista1[1] = "feijão";
+lista1[2] = "macarrão";
+lista1[3] = "leite";
 
-function viraVermelho() {
-    let div = document.getElementById("teste");
-    div.style.backgroundColor = "red";
-}
+//obter valor posição do array
+let x = lista1[3];
+alert(x);
 
-function viraAzul() {
-    let div = document.getElementById("teste");
-    div.style.backgroundColor = "blue";
-}
+//alterar valor, aponta posição -> valor
+lista1[0] = "café";
+alert(lista1[0]);
 
-function mudou() {
-    console.log("mudoou");
-}
+//Mostrando array completo
+//console.log(lista1)
 
-function teclaPressionada() {
-    let input = document.getElementById("campoTexto").value;
-    console.log(input);
-}
+
+const pessoa = ["Dimitri", "Teixeira", 30]; //array
+
+alert(pessoa.length); //3, tamanho do array
+
+//Pegar ultimo valor de uma array, tamanho total "length", tira -1 item
+alert(pessoa[pessoa.length - 1]);
+
+//acrescenta item no final do array
+pessoa.push("Professor");
+console.log(pessoa);
+
+//verificar se a variavel é uma matriz "true or false"
+alert(Array.isArray(pessoa));
+
+
+
+
+
+
+
+
